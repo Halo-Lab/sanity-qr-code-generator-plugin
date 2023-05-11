0456e961085e3d20988b7a650a1f29f1a5044f1d
Ihor Bozhuk <88133512+Jotunn1@users.noreply.github.com>
2023-05-11 12:21:47 +0300

    renamed local plugin version

--- sanity.config.js
+++ sanity.config.js
@@ -2,7 +2,10 @@ import {defineConfig} from 'sanity'
 import {deskTool} from 'sanity/desk'
 import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './schemas'
-import {QrCodeGenerator} from './plugins/qr-code-generator/'
+import {QrCodeGenerator} from './plugins/previous-version/'
+import {pageSpeedMonitoringPlugin} from 'sanity-plugin-page-speed-monitoring-test'
+
+const API_KEY = process.env.PAGE_SPEED_INSIGHTS_API_KEY
 
 export default defineConfig({
   name: 'default',
@@ -11,7 +14,12 @@ export default defineConfig({
   projectId: process.env.SANITY_STUDIO_PROJECT_ID,
   dataset: process.env.SANITY_STUDIO_DATASET,
 
-  plugins: [deskTool(), visionTool(), QrCodeGenerator()],
+  plugins: [
+    deskTool(),
+    visionTool(),
+    QrCodeGenerator(),
+    pageSpeedMonitoringPlugin({API_KEY: API_KEY}),
+  ],
 
   schema: {
     types: schemaTypes,
