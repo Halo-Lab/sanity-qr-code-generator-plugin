773511a99348065e2609c61767adc3e88c814029
Ihor Bozhuk <88133512+Jotunn1@users.noreply.github.com>
2023-05-17 18:09:31 +0300

    reduce qr-code-image max size

--- plugins/qr-code-generator/src/components/QRCodePreview/index.tsx
+++ plugins/qr-code-generator/src/components/QRCodePreview/index.tsx
@@ -5,12 +5,12 @@ import QRCode from 'react-qr-code'
 const ImageWrapper = styled.div.attrs((props: {size: number}) => props)`
   width: ${(props) => props.size}px;
   height: ${(props) => props.size}px;
-  max-width: 500px;
-  max-height: 500px;
+  max-width: 400px;
+  max-height: 400px;
 
   > * {
-    max-width: 500px;
-    max-height: 500px;
+    max-width: 400px;
+    max-height: 400px;
   }
 `
 
