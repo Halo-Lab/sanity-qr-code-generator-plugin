b69e58d06bae06e665ba12fb83da8c67772f2cd5
Ihor Bozhuk <88133512+Jotunn1@users.noreply.github.com>
2023-06-20 17:46:40 +0300

    updated sanity config

--- sanity.config.js
+++ sanity.config.js
@@ -2,10 +2,7 @@ import {defineConfig} from 'sanity'
 import {deskTool} from 'sanity/desk'
 import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './schemas'
-// import {QrCodeGenerator} from './plugins/qr-code-generator/src'
-import {QrCodeGenerator} from '223300-qr'
-
-// const API_KEY = process.env.PAGE_SPEED_INSIGHTS_API_KEY
+import {QrCodeGenerator} from 'sanity-qr-code-generator'
 
 export default defineConfig({
   name: 'default',
@@ -14,7 +11,16 @@ export default defineConfig({
   projectId: process.env.SANITY_STUDIO_PROJECT_ID,
   dataset: process.env.SANITY_STUDIO_DATASET,
 
-  plugins: [deskTool(), visionTool(), QrCodeGenerator()],
+  plugins: [deskTool(), QrCodeGenerator()],
+  form: {
+    image: {
+      assetSources: (previousAssetSources, {schema}) => {
+        console.log(schema)
+        return previousAssetSources
+      },
+      qrCode: {},
+    },
+  },
 
   schema: {
     types: schemaTypes,
